826d935c6b32e6f1935f1c4b7e056970e21888ef
Akhtar Sayyad <asayyad3@dxc.com>
2020-01-05 13:38:20 -0600

    static changes

--- pywebapp/settings.py
+++ pywebapp/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'site/wwwroot/pywebapp/imfwebapp/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
