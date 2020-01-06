d37d711dde2a9ad9ac1fe597ae32776cc7044ba6
Akhtar Sayyad <asayyad3@dxc.com>
2020-01-05 19:16:53 -0600

    send Project

--- pywebapp/settings.py
+++ pywebapp/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pywebapp.urls'
@@ -121,6 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = "/static/"
+STATIC_URL = "site/wwwroot/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "site/wwwroot/static")
